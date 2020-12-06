09277dd8e4da9f8fb205beec02d75c3e897e42d3
dmb <d.babiak@gmail.com>
2020-12-06 15:36:18 -0500

    PathToLines

--- utils.go
+++ utils.go
@@ -1,9 +1,24 @@
 package dbgo
 
-import "log"
+import (
+	"bufio"
+	"log"
+	"os"
+)
 
 func Check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func PathToLines(pathname string) []string {
+	f, err := os.Open(pathname)
+	Check(err)
+	lines := []string{}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
 }
